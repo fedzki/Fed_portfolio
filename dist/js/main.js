6c148ae72b9c8f45ab36e0ecbe6f384b2b79e611
Lew Frederick Jovellano <fedzki.j@gmail.com>
2019-02-12 08:06:26 +0800

    'project gallery'

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,12 +4,21 @@ const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
+const gallery = document.querySelector("project-gallery");
+const currentPic = document.querySelector("current-pic");
+const projectPics = document.querySelector("project-pics");
+const projectSMpic = document.querySelectorAll("sm-pic");
+const galButtonLight = document.querySelectorAll(".btn-light");
+const galButtonDark = document.querySelectorAll(".btn-dark");
+const galItems = document.querySelectorAll(".gal-items");
 
 // Set Initial Sate of Menu
 
 let showMenu = false;
+let showGallery = false;
 
 menuBtn.addEventListener("click", toggleMenu);
+galItems.addEventListener("click", toggleGallery);
 
 function toggleMenu() {
   if (!showMenu) {
@@ -30,5 +39,10 @@ function toggleMenu() {
 
     //Set Menu State
     showMenu = false;
+  }
+}
+
+function toggleGallery() {
+  if (!showGallery) {
   }
 }
